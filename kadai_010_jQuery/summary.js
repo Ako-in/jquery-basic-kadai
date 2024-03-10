0c9e1061c7a06d671dfc8a42946a30ab337ba171
Ako-in <sinba.805i@gmail.com>
2024-03-10 16:06:46 +0700

    kadai_010ファイルをアップロード

---
+++ kadai_010_jQuery/summary.js
@@ -0,0 +1,28 @@
+// 色が変わる（色は任意）
+// 文字が切り替わる（内容は任意）
+// フェードアウトで文字が消える
+// フェードインで文字が現れる
+
+$(function(){
+  // 色が変わる（色は任意）
+  $('#change-color').on('click', function(){
+    $('#target').css('color','red');
+  });
+
+  // 文字が切り替わる（内容は任意）
+  $('#change-text').on('click', function(){
+    $('#target').text('Hello World!');
+  });
+  
+  // フェードアウトで文字が消える
+  $('#fade-out').on('click', function(){
+    $('#target').fadeOut();
+
+  });
+
+  // フェードインで文字が現れる
+  $('#fade-in').on('click', function(){
+    $('#target').fadeIn();
+  });
+
+});
